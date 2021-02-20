cef500e37b19ebaccadf4819a04f3bf5a0b6ae69
Lukie Kang <lukiek@gmail.com>
2021-02-20 11:31:44 +0000

    stuff. This is my own app so I cna write what he hell id like to in here as its not rocket science in this particualr use case

--- src/App.js
+++ src/App.js
@@ -9,30 +9,30 @@ function App() {
       <Header />
 
       <main className="layout">
-        <h1>Welcome Agent</h1>
+        <h1>Welcome Agent Stoneman</h1>
     
         <p>
          If you are reading this you have been able to receive our coded message. We know of your talents dealing with world crises. Now we must prepare you for another mission.
-          Below are a series of challenges that will train your mind to do stuff.
+          Below are a series of challenges that will train your mind to do stuff. I need some help with the copy here :)
         </p>
 
      
        
 
         <h2>Key #1</h2>
-        <ChallengeCard name="Example Challenge" text="This is the text of the challenge" answer="the answer" code="code that is revealed"/>
+        <ChallengeCard name="Example Challenge" text="This is the text of the challenge, the answer is 'a'" answer="a" code="coded directions 1"/>
   
         <h2>Key #2</h2>
-        <ChallengeCard name="Example Challenge" text="This is the text of the challenge" answer="the answer" code="code that is revealed"/>
+        <ChallengeCard name="Example Challenge" text="This is the text of the challenge,the answer is 'b'" answer="b" code=" coded directions 2"/>
   
         <h2>Key #3</h2>
-        <ChallengeCard name="Example Challenge" text="This is the text of the challenge" answer="the answer" code="code that is revealed"/>
+        <ChallengeCard name="Example Challenge" text="This is the text of the challenge, the answer is 'c'" answer="c" code="coded directions 3"/>
   
         <h2>Key #4</h2>
-        <ChallengeCard name="Example Challenge" text="This is the text of the challenge" answer="the answer" code="code that is revealed"/>
+        <ChallengeCard name="Example Challenge" text="This is the text of the challenge,the answer is 'd'" answer="d" code="coded directions 4"/>
   
         <h2>Key #5</h2>
-        <ChallengeCard name="Example Challenge" text="This is the text of the challenge" answer="the answer" code="code that is revealed"/>
+        <ChallengeCard name="Example Challenge" text="This is the text of the challenge, the answer is 'e'" answer="e" code="coded directions 5"/>
   
         
         
